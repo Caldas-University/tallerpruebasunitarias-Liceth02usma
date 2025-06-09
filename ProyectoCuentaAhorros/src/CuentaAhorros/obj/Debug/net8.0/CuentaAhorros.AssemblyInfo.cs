7214714e0b1f0a89c553124b550031dd30de6727
Liceth02usma <juanitausma02@gmail.com>
2025-06-09 15:08:39 -0500

    Correccion de pruebas

--- ProyectoCuentaAhorros/src/CuentaAhorros/obj/Debug/net8.0/CuentaAhorros.AssemblyInfo.cs
+++ ProyectoCuentaAhorros/src/CuentaAhorros/obj/Debug/net8.0/CuentaAhorros.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CuentaAhorros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a3cdc16255aab545acca9eb2ddbdce294ec2b12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7c77e7f081170509333ecd4cc3cb63497c3550")]
 [assembly: System.Reflection.AssemblyProductAttribute("CuentaAhorros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CuentaAhorros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
